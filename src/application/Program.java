e78fb3cfeaa02ce9ac35060a9296fd950482d58d
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-24 23:21:50 -0300

    primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Operações importantes com data-hora
+		// Converter data-hora global, timezone(sistema local) -> data-hora local
+		// Obter dados de uma data-hora local -> data hora local -> dia, mes, ano, horario
+		// Cálculos com data-hora -> data-hora +/- tempo -> data-hora, data hora 1, data hora 2 -> duração
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
