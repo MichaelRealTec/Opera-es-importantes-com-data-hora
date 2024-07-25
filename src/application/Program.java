15271dcb8bdb872b01d1441ef22952cded178d7a
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-24 23:43:39 -0300

    converter data local e global

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,22 +22,40 @@ public class Program {
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
+		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault()); // LocalDateTime do Brasil
+
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal")); // LocalDateTime de portugal
 		
-		for(String s : ZoneId.getAvailableZoneIds()) {
-			System.out.println(s);
-		}
-		// ZoneId.getAvailableZoneIds(); 
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
 		
+
 		
 		System.out.println("d04 = " + d04);
 		System.out.println("d05 = " + d05);
 		System.out.println("d06 = " + d06);
+		System.out.println("r1 = " + r1);
+		System.out.println("r2 = " + r2);		
+		System.out.println("r3 = " + r3);
+		System.out.println("r4 = " + r4);		
 		
 		
+		System.out.println("d04 dia = " +d04.getDayOfMonth());
+		System.out.println("d04 mês = " +d04.getMonthValue());
+		System.out.println("d04 ano = " +d04.getYear());
 		
-		
-		
+		System.out.println("d05 hora = " +d05.getHour());
+		System.out.println("d05 minutos = " +d05.getMinute());
+						
 		sc.close();
+		
+		/*
+		 		
+		for(String s : ZoneId.getAvailableZoneIds()) {
+			System.out.println(s);
+		}
+		// ZoneId.getAvailableZoneIds();  
+		 */
 	}
 
 }
